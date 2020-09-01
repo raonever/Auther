b90da82c80851bb448735a948f4e9a8c2ee45cdf
raonever <ever29lkh@gmail.com>
2020-09-01 16:38:48 +0900

    sub 411 Level 4 bcrypt salt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");         // 1-1) express, bady-parser, ejs np
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+
+const bcrypt = require("bcrypt");    // bcrypt
+const saltRounds = 10;
 
 const app = express();    // 1-2) app 선언, ejs 셋팅, bodyParser, express 설정
 
@@ -39,36 +41,41 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)      // 암호화
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {    // bcrypt
+    const newUser = new User({
+      email: req.body.username,
+      password: hash     // 암호화
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  })
-})
+});
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);    // 복호화
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){   // 찾을 때 복호화
     if (err){
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {     // bcrypt, 복호화
+          if (result === true) {
+              res.render("secrets");
+          }
+        });
       }
     }
-  })
-})
+  });
+});
 
 app.listen(3000, function() {                 // 1-3) 포트 설정
   console.log("Server started on port 3000");
